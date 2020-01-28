93619cd2d957ba4fee80631f89336a21ca9cb05b
eemeliranta <50950050+eemeliranta@users.noreply.github.com>
2020-01-28 14:15:09 +0200

    Updated requirements.txt, added a script to run open venv and run server

--- runserver.py
+++
@@ -1,6 +0,0 @@
-import os
-print('Starting django server:')
-os.system('python manage.py runserver 0.0.0.0:8000')
-#os.system('python manage.py runserver 0.0.0.0:80')
-
-input("Press Enter to continue...")
